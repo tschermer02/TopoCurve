cc326fe9c7dd713fe319df167726c8b85da72a06
Jxnash <joel.nash816@gmail.com>
2023-10-24 15:31:09 -0600

    adding dem_rasterio.py

---
+++ inital_dem/dem_rasterio.py
@@ -0,0 +1,28 @@
+# pip install rasterio
+# pip install numpy
+# pip install matplotlib
+# pip install scipy
+# 
+
+import numpy as np
+import rasterio as rio
+from rasterio.plot import show
+import matplotlib.pyplot as plt
+from scipy import signal
+
+dem = rio.open("C:/Users/Joeln/source/repos/TopoCurve/DEM_files/Durango_Clip.tif")
+# dem_array = dem.read(1).astype('float64')
+
+dem_array = dem.read()
+print(dem_array)
+
+dx = abs(dem_array[0][0][0] -dem_array[0][0][1])
+print(dx)
+
+dy = abs(dem_array[0][0][0] -dem_array[0][1][0])
+print(dy)
+
+print(dem_array.shape)
+
+dem_array_detrend = signal.detrend(dem_array)
+print(dem_array_detrend)
