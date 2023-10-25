94a931958e8d8e7d024252ef1e4c1329cb89b7db
Taylor Schermer <tschermer02@gmail.com>
2023-10-25 08:07:23 -0600

    Changed name for geotiff class

--- inital_dem/dem_geotiff.py
+++ inital_dem/dem_geotiff.py
@@ -7,32 +7,17 @@ from PIL import Image
 import numpy as np
 from scipy import signal
 
+from dem_class import Dem_Class
+
 tiff_file = "C:/Users/tsche/source/repos/TopoCurve/DEM_files/Durango_Clip.tif"
 
 bounding_box = [(50.0, -10.8), (50.3, -10.9)]
 geoTiff = GeoTiff(tiff_file)
 array = geoTiff.read()
-
-
-zarray = np.array(array)
-
-dx = abs(zarray[(0,0)]-zarray[(0,1)])
-dy = abs(zarray[(0,0)]-zarray[(1,0)])
-
-print(zarray)
-
-print(dx)
-print(dy)
-
-
 dimx,dimy = geoTiff.tif_shape
 
-print(dimx)
-print(dimy)
+dem1 = Dem_Class(array, dimx, dimy)
 
-Z_detrended = signal.detrend(zarray)
-
-plane = zarray-Z_detrended
-
-print(Z_detrended)
-print(plane)
+print(dem1.z_array)
+print(dem1.dx_dy())
+print(dem1.detrend())
