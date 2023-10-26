977ab848eed8326b683a644c096582cf3a730aad
Taylor Schermer <tschermer02@gmail.com>
2023-10-26 16:09:55 -0600

    tukey window notes

--- inital_dem/spectral_filtering.py
+++ inital_dem/spectral_filtering.py
@@ -1,3 +1,6 @@
 # Data Visualization: How to check to see if the code is working ... plots
-# Image processing tools: OpenCV?
+# Image processing tools: OpenCV? https://scikit-image.org/? Pillow?
 # Plotting: matlibplt
+#Define of find a 2D tukey window, maybe signal processing package: https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.windows.tukey.html
+#Tukey window: https://photutils.readthedocs.io/en/stable/api/photutils.psf.matching.TukeyWindow.html
+
