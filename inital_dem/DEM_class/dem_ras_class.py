cf8517836769f4cc93ae75effa7fee69ac0c3509
Jxnash <joel.nash816@gmail.com>
2023-10-31 16:36:05 -0600

    adding print and mirror function method to ras class

--- inital_dem/DEM_class/dem_ras_class.py
+++ inital_dem/DEM_class/dem_ras_class.py
@@ -1,25 +1,43 @@
 import rasterio as rio
 from scipy import signal
+import numpy as np
+from rasterio.plot import show
+import matplotlib.pyplot as plt
 
 class Dem_Ras_Class():
     def __init__(self, dem_path):
         dem = rio.open(dem_path)
-        self.z_array = dem.read().astype('float64')
+        self.z_array = dem.read(1).astype('float64')
 
 
     def dimx_dimy(self):
-        dim_x =  self.z_array.shape[1]
-        dim_y =  self.z_array.shape[2]
+        dim_x =  self.z_array.shape[0]
+        dim_y =  self.z_array.shape[1]
         return dim_x, dim_y
 
 
     def dx_dy(self):
-        dx = abs(self.z_array[0][0][0] -self.z_array[0][0][1])
-        dy = abs(self.z_array[0][0][0] -self.z_array[0][1][0])
+        dx = abs(self.z_array[0][0] -self.z_array[0][1])
+        dy = abs(self.z_array[0][0] -self.z_array[1][0])
         return dx,dy
 
 
-    def detrend(self):
-        Z_detrended = signal.detrend(self.z_array)
-        plane = self.z_array-Z_detrended
-        return Z_detrended, plane
+    def detrended(self):
+        self.detrend = signal.detrend(self.z_array)
+        plane = self.z_array-self.detrend
+        return self.detrend, plane
+    
+    def plot_func(self, input):
+        fig, ax = plt.subplots(1, figsize = (12,12))
+        show(input, cmap='Greys_r', ax=ax)
+        plt.axis("off")
+        plt.show()
+
+    def mirror_array(self):
+        top = np.concatenate((np.rot90(self.detrend,2), np.flipud(self.detrend), np.rot90(self.detrend,2)), axis = 1)
+        mid = np.concatenate((np.fliplr(self.detrend), self.detrend, np.fliplr(self.detrend)), axis =1)
+        bot = np.concatenate((np.rot90(self.detrend,2), np.flipud(self.detrend), np.rot90(self.detrend,2)), axis =1)
+
+        Zm = np.concatenate((top, mid, bot), axis=0)
+        return Zm
+        
--- inital_dem/DEM_class/dem_rasterio.py
+++ inital_dem/DEM_class/dem_rasterio.py
@@ -15,45 +15,24 @@ from dem_ras_class import Dem_Ras_Class
 
 dem_test = Dem_Ras_Class("C:/Users/Joeln/source/repos/TopoCurve/DEM_files/Durango_Clip.tif")
 
-z_detrended, plane = dem_test.detrend()
+z_detrended, plane = dem_test.detrended()
 
-'''
 print(dem_test.z_array)
 print(dem_test.dx_dy())
 print(dem_test.dimx_dimy())
-print(dem_test.detrend())
+print(dem_test.detrended)
 
 
-fig, ax = plt.subplots(1, figsize=(12, 12))
-show(z_detrended, cmap='Greys_r', ax=ax)
-show(z_detrended, contour=True,  linewidths=0.7)
+
+
+dem_test.plot_func(dem_test.mirror_array())
+
+
+
+'''
+fig, ax = plt.subplots(1, figsize = (12,12))
+show(Zm, cmap='Greys_r', ax=ax)
 plt.axis("off")
 plt.show()
 '''
 
-
-rtc = np.rot90(z_detrended,2)
-ts = np.flipud(z_detrended) 
-ltc = np.rot90(z_detrended,2)
-ls = np.fliplr(z_detrended)
-cen = z_detrended
-rs = np.fliplr(z_detrended)
-rbc = np.rot90(z_detrended,2)
-bs = np.flipud(z_detrended)
-lbc = np.rot90(z_detrended,2)
-
-# rtc, ts, ltc, ls, cen, rs, rbc, bs , lbc
-top = np.concatenate((rtc, ts, ltc), axis = None)
-mid = np.concatenate((ls, cen, rs), axis = None)
-bot = np.concatenate((rbc, bs, lbc), axis = None)
-
-Zm = np.vstack((top, mid, bot), axis = 0)
-# Zm.extend(ts); Zm.extend(ltc); Zm.extend(ls); Zm.extend(cen); Zm.extend(rs); Zm.extend(rbc); Zm.extend(bs); Zm.extend(lbc);
-
-print(z_detrended.size)
-print(Zm.size)
-
-fig, ax = plt.subplots(1, figsize=(12, 12))
-show(Zm, cmap='Greys_r', ax=ax)
-plt.axis("off")
-plt.show()
