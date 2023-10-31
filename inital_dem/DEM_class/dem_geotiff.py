4d8839dd42eb8d3fb94eaedff5cb290d88ef784d
Taylor Schermer <tschermer02@gmail.com>
2023-10-31 16:23:59 -0600

    added plot and mirror method to dem_geotiff class

--- inital_dem/DEM_class/dem_geotiff.py
+++ inital_dem/DEM_class/dem_geotiff.py
@@ -20,19 +20,9 @@ dem = Dem_Class(array, dimx, dimy)
 detrended, plane = dem.detrend()
 
 #Plotting detrended DEM
-img_array = 255*((detrended - np.amin(detrended))/(np.amax(detrended)- np.amin(detrended)))
-Image.fromarray(img_array).convert("RGB").save("greyscale_dem_detrend.png")
+dem.plot(detrended, "greyscale_dem_detrend.png")
 
 #Mirroring DEM on all sides
-f_array = []
-top = np.concatenate((np.rot90(detrended,2),np.flipud(detrended),np.rot90(detrended,2)), axis=1)
-middle = np.concatenate((np.fliplr(detrended),detrended,np.fliplr(detrended)), axis=1)
-bottom = np.concatenate((np.rot90(detrended,2),np.flipud(detrended),np.rot90(detrended,2)), axis=1)
+dem.plot(dem.mirror_dem(), "greyscale_dem_mirror.png")
 
-f_array = np.concatenate((top, middle, bottom), axis=0)
-
-img_array_Zm = 255*((f_array - np.amin(f_array))/(np.amax(f_array)- np.amin(f_array)))
-Image.fromarray(img_array_Zm).convert("RGB").save("greyscale_dem_detrend_Zm.png")
-
-print(detrended)
 
--- inital_dem/DEM_class/dem_geotiff_class.py
+++ inital_dem/DEM_class/dem_geotiff_class.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import signal
+from PIL import Image
 
 class Dem_Class():
     def __init__(self, array, dimx, dimy):
@@ -14,6 +15,19 @@ class Dem_Class():
         return dx,dy
 
     def detrend(self):
-        Z_detrended = signal.detrend(self.z_array)
-        plane = self.z_array-Z_detrended
-        return Z_detrended, plane
+        self.Z_detrended = signal.detrend(self.z_array)
+        plane = self.z_array-self.Z_detrended
+        return self.Z_detrended, plane
+    
+    def plot(self, input, filename):
+        img_array = 255*((input - np.amin(input))/(np.amax(input)- np.amin(input)))
+        Image.fromarray(img_array).convert("RGB").save(filename)
+    
+    def mirror_dem(self):
+        f_array = []
+        top = np.concatenate((np.rot90(self.Z_detrended,2),np.flipud(self.Z_detrended),np.rot90(self.Z_detrended,2)), axis=1)
+        middle = np.concatenate((np.fliplr(self.Z_detrended),self.Z_detrended,np.fliplr(self.Z_detrended)), axis=1)
+        bottom = np.concatenate((np.rot90(self.Z_detrended,2),np.flipud(self.Z_detrended),np.rot90(self.Z_detrended,2)), axis=1)
+        f_array = np.concatenate((top, middle, bottom), axis=0)
+        return f_array
+
