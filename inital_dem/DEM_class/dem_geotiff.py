0b48b083a909021d73cb06aba6986e6d556ff94e
Taylor Schermer <tschermer02@gmail.com>
2023-11-07 17:19:51 -0700

    Added padding to array

--- inital_dem/DEM_class/dem_geotiff.py
+++ inital_dem/DEM_class/dem_geotiff.py
@@ -23,33 +23,14 @@ detrended, plane = dem.detrend()
 dem.plot(detrended, "greyscale_dem_detrend.png")
 
 # Mirroring DEM on all sides
-dem.plot(dem.mirror_dem(), "greyscale_dem_mirror.png")
 
+dimx_f,dimy_f, mirror = dem.mirror_dem()
+dem.plot(mirror, "greyscale_dem_mirror.png")
+
+print(dimx_f,dimy_f)
 # Padding array
 
-#Python program to find
-#smallest power of 2
-#greater than or equal to n
-import math
+dem.plot(dem.padding(), "greyscale_dem_padding.png")
 
-# Function to find the smallest power of 2
-# greater than or equal to n
-def nearestPowerOf2(N):
-	# Calculate log2 of N
-	a = int(math.log2(N))
-
-	# If 2^a is equal to N, return N
-	if 2**a == N:
-		return N
-	
-	# Return 2^(a + 1)
-	return 2**(a + 1)
-
-# Main function
-if __name__ == "__main__":
-	# Input number
-	n = 5
-	# Call the nearestPowerOf2 function
-	print(nearestPowerOf2(n))
 
 # https://www.geeksforgeeks.org/numpy-pad-function-in-python/
