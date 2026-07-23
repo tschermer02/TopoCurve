faa5f495e49acfee2db6b6e89414334de3a94fa7
ntklema <147839233+ntklema@users.noreply.github.com>
2026-07-23 10:31:06 -0700

    Create Scale_Space_Curvature_Analysis.py

---
+++ src/test_scripts/Dome_Analysis/Scale_Space_Curvature_Analysis.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Nate Klema - 6/23/26
+
+Curvature analysis of exfoliation domes in NC, SC, and CA 
+
+Inputs: 
+    DEM raster
+    Polygon shapefile with region of interest (clip of dome)
+    Shapefile with sample points
+"""
+
+ 
