ca95628e33406bd6d05a5cff0943fedb8b8b838d
ntklema <147839233+ntklema@users.noreply.github.com>
2026-08-17 18:09:07 -0600

    Update Scale_Space_Curvature_Analysis.py
    Add script for scale-space curvature calculation

--- src/test_scripts/Dome_Analysis/Scale_Space_Curvature_Analysis.py
+++ src/test_scripts/Dome_Analysis/Scale_Space_Curvature_Analysis.py
@@ -21,9 +21,9 @@ import matplotlib.animation as animation
 
 
 #%% ---------------- Import datasets ---------------------
-tiff_file = "/Users/ntklema/Library/CloudStorage/OneDrive-FortLewisCollege/Research_Projects/Dome Exfoliation/Dome_Exfoliation_2/DEMs/40_Acre_Rock.tif"
+tiff_file = "/Users/ntklema/Library/CloudStorage/OneDrive-FortLewisCollege/Research_Projects/Dome Exfoliation/Yosemite/Clipped_Rasters/Olmsted_Upper.tif"
 
-shapefile="/Users/ntklema/Library/CloudStorage/OneDrive-FortLewisCollege/Research_Projects/Dome Exfoliation/Dome_Exfoliation_2/Shapefiles/40_Acre_Rock.shp"
+shapefile="/Users/ntklema/Library/CloudStorage/OneDrive-FortLewisCollege/Research_Projects/Dome Exfoliation/Yosemite/Shapefiles/Olmsted_Upper.shp"
 s_points = gpd.read_file(shapefile)
 x = np.array(s_points.geometry.x)
 
@@ -35,8 +35,8 @@ spectral_filter = SpectralFiltering(tiff_file)
 
 
 #%% Build filter 
-ns=20
-df=5
+ns=200
+df=1
 KM = np.full((len(x),ns), np.nan)
 F=np.array([1,6])
 f=np.full(ns,np.nan)
@@ -45,7 +45,7 @@ start_time = time.perf_counter()  # Record start time
 for i in range(ns):
     dx, dy, filtered_elevation = spectral_filter.FFT(F, 'lowpass', 0)
     K = dem.CurveCalc(filtered_elevation, dx, dy, 0)
-    Att,X,Y=dem.TopoCurve_Sample(shapefile,K[6],shapefile_attributes=["Avg"])
+    Att,X,Y=dem.TopoCurve_Sample(shapefile,K[6],shapefile_attributes=["Sheet Thic"])
     
     KM[:,i]=Att["KM"]
     f[i]=F[1]
@@ -55,24 +55,26 @@ for i in range(ns):
     print(f"Iteration: {i+1} of {ns}")
     print(f"Elapsed time: {elapsed:0.0f} seconds\n")
 
-
+Att['f']=f
+Att['KM_Array']=KM
 #%%
-for i in range(len(x)):
-    plt.plot(f,((KM[i,:])),color='k',linewidth=0.5)
+# for i in range(len(x)):
+#     plt.plot(f,((KM[i,:])),color='k',linewidth=0.5)
     
 plt.plot(f,(np.median((KM),axis=0)),color='r')
 plt.show()
 #%%
-plt.scatter(Att['Avg'],np.median(KM,axis=1))
+plt.scatter(Att['Sheet Thic'],np.median(KM,axis=1))
 
 #%%
 
-x_a=Att['Avg']
+x_a=Att['Sheet Thic']
 y_a=KM
 fig, ax = plt.subplots()
 scat = ax.scatter(x_a,y_a[:, 0])
+plt.axhline(0,color='k',linewidth=0.5)
 ax.set_xlim(0,100)
-ax.set_ylim(-0.01,0.01)
+ax.set_ylim(-0.05,0.05)
 
 def update(frame):
     scat.set_offsets(np.c_[x_a, y_a[:, frame]])
@@ -81,9 +83,19 @@ def update(frame):
 ani = animation.FuncAnimation(fig, update, frames=ns, interval=1, blit=True)
 
 # Save as gif (no extra install needed)
-ani.save('scatter.gif', writer='pillow', fps=1)
+# ani.save('scatter.gif', writer='pillow', fps=1)
 
 # Or save as mp4 (requires: brew install ffmpeg)
-ani.save('scatter.mp4', writer='ffmpeg', fps=20)
+ani.save('/Users/ntklema/Library/CloudStorage/OneDrive-FortLewisCollege/Research_Projects/Dome Exfoliation/Dome_Exfoliation_2/Animations/Puppy_0to200.mp4', writer='ffmpeg', fps=6)
 
 plt.show()
+
+#%% Save dictionary
+
+import pickle
+
+with open('/Users/ntklema/Library/CloudStorage/OneDrive-FortLewisCollege/Research_Projects/Dome Exfoliation/Dome_Exfoliation_2/Python_Dictionaries/Puppy.pkl', 'wb') as r:
+    pickle.dump(Att, r)
+
+with open('/Users/ntklema/Library/CloudStorage/OneDrive-FortLewisCollege/Research_Projects/Dome Exfoliation/Dome_Exfoliation_2/Python_Dictionaries/Twaine_Harte_West.pkl', 'rb') as r:
+    my_dict = pickle.load(r)
